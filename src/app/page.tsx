9156ebf3be36eef9cf74ca098da744ac01016ed3
k-suke39 <pz.mozarla@gmail.com>
2023-10-07 11:45:26 +0900

    ⚡️ cardの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Link href="https://twitter.com/share?url">Twitter</Link>
+			<Link href="https://twitter.com/share?url=next-ogp-analytics.vercel.app">
+				Twitter
+			</Link>
 		</main>
 	);
 }
