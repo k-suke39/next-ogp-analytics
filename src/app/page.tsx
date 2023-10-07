0e50f80fed06642efdac33a6eb016d75e022bed4
k-suke39 <pz.mozarla@gmail.com>
2023-10-07 11:52:02 +0900

    ⚡️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Link href="https://twitter.com/share?url=next-ogp-analytics.vercel.app">
+			<Link href="https://twitter.com/share?url=https://next-ogp-analytics.vercel.app">
 				Twitter
 			</Link>
 		</main>
